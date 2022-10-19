9aeae14d6092db21c34de8014c005e5204fd2e66
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2022-10-18 18:45:37 -0600

    Sign main page, format all documents.

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const emailInput = document.querySelector(".email"); 
+const emailInput = document.querySelector(".email");
 const errorMessage = document.querySelector(".error-message");
 const errorIcon = document.querySelector(".error-icon");
 
-emailInput.addEventListener('change', ()=> {
-    let email = emailInput.value; 
-    if (email.match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i) === null) {
-        errorMessage.classList.toggle("hidden");
-        errorIcon.classList.toggle("hidden");
-    } else {
-        errorMessage.classList.add("hidden");
-        errorIcon.classList.add("hidden");
-    }
-        
-    }
-);
-
+emailInput.addEventListener("change", () => {
+  let email = emailInput.value;
+  if (
+    email.match(
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    ) === null
+  ) {
+    errorMessage.classList.toggle("hidden");
+    errorIcon.classList.toggle("hidden");
+  } else {
+    errorMessage.classList.add("hidden");
+    errorIcon.classList.add("hidden");
+  }
+});

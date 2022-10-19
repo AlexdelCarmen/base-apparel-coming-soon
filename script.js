9354bb689894d079c0c21c917962c91ee31737ea
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2022-10-18 18:54:35 -0600

    Correct script to use button to trigger.

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const emailInput = document.querySelector(".email");
 const errorMessage = document.querySelector(".error-message");
 const errorIcon = document.querySelector(".error-icon");
+const button = document.querySelector("button");
 
-emailInput.addEventListener("change", () => {
+button.addEventListener("click", () => {
   let email = emailInput.value;
   if (
     email.match(
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
     ) === null
   ) {
-    errorMessage.classList.toggle("hidden");
-    errorIcon.classList.toggle("hidden");
+    errorMessage.classList.remove("hidden");
+    errorIcon.classList.remove("hidden");
   } else {
     errorMessage.classList.add("hidden");
     errorIcon.classList.add("hidden");
   }
 });
+
+
